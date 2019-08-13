e60036d55386af58ad06b5ab9faa7e07e735afff
vasantvohra <vasantvvv@gmail.com>
2019-08-13 16:02:17 +0530

    80% accuracy

--- prediction.py
+++ prediction.py
@@ -2,12 +2,12 @@ import os
 import cv2
 import tensorflow as tf
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from keras.preprocessing import image
 
 def predict(img_path):
      labels={0: 'cardboard', 1: 'glass', 2: 'metal', 3: 'paper', 4: 'plastic', 5: 'trash'}
-#img_path = 'C:\\Users\\vvohra\\Downloads\\dataset-original\\dataset-original\\metal\\'
+#img_path = 'C:\\Users\\--\\Downloads\\dataset-original\\dataset-original\\metal\\'
 
      img = image.load_img(img_path, target_size=(300, 300))
      img = image.img_to_array(img, dtype=np.uint8)
@@ -16,8 +16,11 @@ def predict(img_path):
      
      model = tf.keras.models.load_model("trained_model.h5")
      p=model.predict(img[np.newaxis, ...])
-     print(p.shape)
+     
+     print("p.shape:",p.shape)
+     print("prob",np.max(p[0], axis=-1))
      predicted_class = labels[np.argmax(p[0], axis=-1)]
-     os.remove(img_path)
-     print(predicted_class)
+     #os.remove(img_path)
+     print("classified label:",predicted_class)
      return(predicted_class)
+#print(predict(img_path = 'C:\\Users\\--\\Downloads\\dataset-original\\dataset-original\\metal\\metal1.jpg'))
